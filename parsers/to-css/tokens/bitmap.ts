b5626d79eea164048a944eb58170530bea816ee5
Antoine Moreaux <moreaux.antoine@gmail.com>
2020-08-19 17:34:22 +0200

    fix(to-css): update to css parser

--- parsers/to-css/tokens/bitmap.ts
+++ parsers/to-css/tokens/bitmap.ts
@@ -1,6 +1,10 @@
 import { BitmapToken } from '@specifyapp/types';
 
 export class Bitmap extends BitmapToken {
+  constructor(token: Partial<BitmapToken>) {
+    super(token);
+  }
+
   toCss(): string {
     return JSON.stringify(this.value.url);
   }
