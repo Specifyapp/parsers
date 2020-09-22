6110f6d8073c72c866de2444c736dde1dfd8c77e
Antoine Moreaux <moreaux.antoine@gmail.com>
2020-09-22 18:57:21 +0200

    fix(to-css): fix to-css parser

--- parsers/to-css/to-css.parser.ts
+++ parsers/to-css/to-css.parser.ts
@@ -52,7 +52,11 @@ export default async function (
             `${token.type.charAt(0).toUpperCase() + token.type.slice(1)}`
           ](token);
 
-          return `--${transformNameFn(token.name)}: ${instance.toCss(options)};`;
+          const name =
+            options?.formatName || token.name.includes(' ') || token.name.includes('\n')
+              ? transformNameFn(token.name)
+              : token.name;
+          return `--${name}: ${instance.toCss(options)};`;
         })
         .join('');
       return result;
