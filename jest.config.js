78659de03002ba6ffaec5a28f5270a5671bbe08b
Antoine Moreaux <moreaux.antoine@gmail.com>
2020-09-28 22:32:08 +0200

    improvement(badges): add badges

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,9 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   globals: {
+    collectCoverageFrom: ['parsers/**/*.ts', '!parsers/**/*.spec.ts'],
+    collectCoverage: true,
+    coverageReporters: ['lcov'],
     'ts-jest': {},
   },
 };
