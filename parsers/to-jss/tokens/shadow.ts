b080d508caea2a276e29463dc3cccde7e916627b
Allan Michay <allan.michay@gmail.com>
2020-10-28 17:45:37 +0100

    feat(tokens): add spread option in the shadow token

--- parsers/to-jss/tokens/shadow.ts
+++ parsers/to-jss/tokens/shadow.ts
@@ -9,44 +9,63 @@ export class Shadow extends ShadowToken {
 
   toJss({ shadowFormat = 'string', colorFormat = 'rgb' }: FormatTokenType) {
     return this.value.reduce((acc, shadow, idx, arr) => {
-      const { color, offsetX, offsetY, blur, isInner } = shadow;
+      const { color, offsetX, offsetY, blur, isInner, spread } = shadow;
       const x = offsetX.value;
       const y = offsetY.value;
       const bl = blur.value;
-      const innerText = isInner ? {
-        string: 'inset ',
-        array: "'inset', ",
-        object: "inset",
-      }[shadowFormat] : '';
-      const prepend = idx ? {
-        string: acc + ', ',
-        array: acc + '], [',
-        object: acc + ', ',
-      }[shadowFormat] : '';
-      const before = !idx ? {
-        string: "'",
-        array: '[[',
-        object: '[',
-      }[shadowFormat] : '';
-      const after = !arr[idx + 1] ? {
-        string: "'",
-        array: ']]',
-        object: ']',
-      }[shadowFormat] : '';
+      const innerText = isInner
+        ? {
+            string: 'inset ',
+            array: "'inset', ",
+            object: 'inset',
+          }[shadowFormat]
+        : '';
+      const prepend = idx
+        ? {
+            string: acc + ', ',
+            array: acc + '], [',
+            object: acc + ', ',
+          }[shadowFormat]
+        : '';
+      const before = !idx
+        ? {
+            string: "'",
+            array: '[[',
+            object: '[',
+          }[shadowFormat]
+        : '';
+      const after = !arr[idx + 1]
+        ? {
+            string: "'",
+            array: ']]',
+            object: ']',
+          }[shadowFormat]
+        : '';
 
       if (shadowFormat === 'object')
-        return before + prepend + JSON.stringify({
-          x: x.measure,
-          y: y.measure,
-          blur: bl.measure,
-          spread: 0, // TODO add spread value to api results
-          color: tinycolor(color.value).toString(colorFormat),
-          inset: innerText,
-        }) + after;
+        return (
+          before +
+          prepend +
+          JSON.stringify({
+            x: x.measure,
+            y: y.measure,
+            blur: bl.measure,
+            spread: spread?.value || 0,
+            color: tinycolor(color.value).toString(colorFormat),
+            inset: innerText,
+          }) +
+          after
+        );
       else if (shadowFormat === 'array')
-        return `${before}${prepend}${innerText}${x.measure}, ${y.measure}, ${bl.measure}, '${tinycolor(color.value).toString(colorFormat)}'${after}`;
+        return `${before}${prepend}${innerText}${x.measure}, ${y.measure}, ${bl.measure}${
+          spread ? `, ${spread.value.measure}` : ''
+        },'${tinycolor(color.value).toString(colorFormat)}'${after}`;
       else
-        return `${before}${prepend}${innerText}${x.measure}${x.unit} ${y.measure}${y.unit} ${bl.measure}${bl.unit} ${tinycolor(color.value).toString(colorFormat)}${after}`;
+        return `${before}${prepend}${innerText}${x.measure}${x.unit} ${y.measure}${y.unit} ${
+          bl.measure
+        }${bl.unit}${spread ? ` ${spread.value.measure}${spread.value.unit}` : ''} ${tinycolor(
+          color.value,
+        ).toString(colorFormat)}${after}`;
     }, '');
   }
 }
