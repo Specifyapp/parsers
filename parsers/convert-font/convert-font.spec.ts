bfe359f82786ca1f63a80f075e0ccd5e8f09fcc7
Antoine Moreaux <moreaux.antoine@gmail.com>
2020-12-03 14:32:42 +0100

    fix(convert-font): prevent error when font file missing

--- parsers/convert-font/convert-font.spec.ts
+++ parsers/convert-font/convert-font.spec.ts
@@ -6,16 +6,21 @@ import libs from '../global-libs';
 
 describe('convert-font', () => {
   it('Default options', async done => {
-    const fonts = [seeds.tokens.find(({ type }) => type === 'font')] as Array<FontToken>;
+    const fonts = seeds.tokens.filter(({ type }) => type === 'font') as Array<FontToken>;
     const result = await convertFont(fonts, undefined, libs as LibsType);
     if (result instanceof Error) return done.fail(result);
     expect(Array.isArray(result)).toEqual(true);
-    expect(result.length).toEqual(5);
+    expect(result.length).toEqual(fonts.filter(({ value }) => !value.fontFileMissing).length * 5);
     result.forEach(item => {
       expect(typeof item.value.url).toMatch('string');
     });
-    expect(result.map(({ name }) => name)).toEqual(
-      FontFormatList.map(format => `${fonts[0].name}.${format}`),
+    const fontsWithoutMissing = fonts.filter(({ value }) => !value.fontFileMissing);
+    expect(result.map(({ name }, index) => name)).toEqual(
+      fontsWithoutMissing
+        .map(font => {
+          return FontFormatList.map(format => `${font.name}.${format}`);
+        })
+        .flat(2),
     );
     done();
   });
@@ -29,7 +34,7 @@ describe('convert-font', () => {
     const result = await convertFont(fonts, options, libs as LibsType);
     if (result instanceof Error) return done.fail(result);
     expect(Array.isArray(result)).toEqual(true);
-    expect(result.length).toEqual(fonts.length);
+    expect(result.length).toEqual(fonts.filter(({ value }) => !value.fontFileMissing).length);
     result.forEach((item, index) => {
       expect(typeof item.value.url).toMatch('string');
       const expectedName = libs._.kebabCase(
--- tests/global.ts
+++ tests/global.ts
@@ -1,7 +1,9 @@
-import { AllowedFormat, PartialRecord } from '../types';
+import { AllowedFormat, FontToken, PartialRecord } from '../types';
+import { Token } from '../types/tokens';
 import _ from 'lodash';
 import tinycolor from 'tinycolor2';
 import got from 'got';
+import * as seeds from '../seeds.json';
 
 jest.mock('../parsers/global-libs', () => {
   return {
@@ -12,6 +14,14 @@ jest.mock('../parsers/global-libs', () => {
       font: {
         convert: async (payload: { postscriptName: string; formats: Array<AllowedFormat> }) => {
           let result: PartialRecord<AllowedFormat, string> = {};
+          const inSeed = seeds.tokens.find(
+            token =>
+              token.type === 'font' &&
+              (token.value as FontToken['value']).fontPostScriptName === payload.postscriptName,
+          ) as FontToken;
+          if (inSeed && inSeed.value.fontFileMissing) {
+            throw new Error('Source file not found');
+          }
           await Promise.all(
             payload.formats.map(async format => {
               result[format] = `${payload.postscriptName}.${format}`;
