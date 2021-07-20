367108e9a77e584e18f3ae00e4016d68c7a8ddbf
Antoine Moreaux <moreaux.antoine@gmail.com>
2021-07-20 16:05:40 +0200

    chore(list-paths-by-pattern): refacto + add tests

--- libs/list-paths-by-pattern.ts
+++ libs/list-paths-by-pattern.ts
@@ -7,41 +7,32 @@ export default function listPathsByPattern<T extends object>(
   try {
     if (!pattern.includes('[*]')) return has(base, pattern) ? [pattern] : [];
     return pattern
-      .split('[*]')
-      .flatMap(elm => [elm, '[*]'])
-      .slice(0, -1)
-      .reduce(
+      .split(/(?=\[\*])|(?<=\[\*])/g) // split without remove the delimiter
+      .reduce<Array<string>>(
         (paths, item) => {
           if (item === '[*]') {
             return paths
               .map(basePath => {
-                if (basePath !== '' && !has(base, basePath)) return paths;
-                const iteration = Array.isArray(base) ? base : get(base, basePath);
-                if (!iteration?.length) return paths;
+                const iteration = basePath ? get(base, basePath) : base;
+                if (!Array.isArray(iteration)) return [];
                 return Array(iteration.length)
                   .fill(0)
                   .reduce(
-                    (acc, _, index) => {
-                      if (has(base, `${basePath}[${index}]`)) acc.push(`${basePath}[${index}]`);
-                      return acc;
-                    },
-                    [''],
+                    (acc, _, index) =>
+                      has(base, `${basePath}[${index}]`) ? [...acc, `${basePath}[${index}]`] : acc,
+                    [],
                   );
               })
               .flat(2);
           } else {
             return paths.reduce<Array<string>>(
-              (acc, elm) => {
-                if (has(base, `${elm}${item}`)) acc.push(`${elm}${item}`);
-                return acc;
-              },
-              [''],
+              (acc, elm) => (has(base, `${elm}${item}`) ? [...acc, `${elm}${item}`] : acc),
+              [],
             );
           }
         },
         [''],
-      )
-      .slice(1);
+      );
   } catch (err) {
     throw err;
   }
--- tests/tests/libs/list-paths-by-pattern.spec.ts
+++ tests/tests/libs/list-paths-by-pattern.spec.ts
@@ -19,7 +19,7 @@ describe('Libs - list path by pattern', () => {
     return;
   });
 
-  it('Should return ', async () => {
+  it('Should return as many elements as there are in seeds', async () => {
     const pattern = '[*][*].name';
     const base = [
       [
@@ -44,6 +44,94 @@ describe('Libs - list path by pattern', () => {
     const result = listPathsByPattern(base, pattern);
     expect(result.length).toEqual(base.flat(2).length);
     result.forEach((path, index) => expect(_.get(base, path)).toEqual(index + 1));
+    return;
+  });
+
+  it('Should not return any elements', async () => {
+    const pattern = 'name[*]';
+    const base = [
+      {
+        name: 'random-string',
+      },
+
+      {
+        name: 2,
+      },
+    ];
+    const result = listPathsByPattern(base, pattern);
+    expect(result.length).toEqual(0);
+    return;
+  });
+
+  it('Should return elements which exist included undefined, 0 or false ', async () => {
+    const pattern = '[*]name';
+    const base = [
+      {
+        name: false,
+      },
+
+      {
+        name: 0,
+      },
+      {
+        name: undefined,
+      },
+    ];
+    const result = listPathsByPattern(base, pattern);
+    expect(result.length).toEqual(3);
+    return;
+  });
+
+  it('should loop through the matrix to find the bar key and loop through the array to find foo', async () => {
+    const pattern = '[*][*].bar[*].foo';
+    const token = [
+      [
+        {
+          bar: 1,
+        },
+        {
+          foo: 1,
+        },
+      ],
+      [
+        {
+          foo: 1,
+        },
+        {
+          bar: [
+            {
+              foo: 2,
+            },
+          ],
+        },
+      ],
+    ];
+    const result = listPathsByPattern(token, pattern);
+    const expectedResult = ['[1][1].bar[0].foo'];
+    expect(result).toEqual(expectedResult);
+    return;
+  });
+
+  it('should work with a pattern that include specific index instead of *', async () => {
+    const pattern = '[0][*].bar';
+    const token = [
+      [
+        {
+          bar: 1,
+        },
+        {
+          bar: 2,
+        },
+      ],
+      [
+        {
+          bar: 3,
+        },
+      ],
+    ];
+    const result = listPathsByPattern(token, pattern);
+    const expectedResult = ['[0][0].bar', '[0][1].bar'];
+    expect(result).toEqual(expectedResult);
     return;
   });
 });
