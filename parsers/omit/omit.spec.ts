c23ced797c1a24a377a10b2d1d7c682d1a370432
Antoine Moreaux <moreaux.antoine@gmail.com>
2021-09-07 18:17:37 +0200

    feat(omit): add parser omit

---
+++ parsers/omit/omit.spec.ts
@@ -0,0 +1,57 @@
+import seeds from '../../tests/seeds';
+import omit from './omit.parser';
+import libs from '../global-libs';
+import { has } from 'lodash';
+
+describe('Omit', () => {
+  it('Execute parser', async () => {
+    const result = await omit(
+      seeds().tokens,
+      {
+        keys: ['value.color'],
+      },
+      libs,
+    );
+    if (result instanceof Error) return fail(result);
+    result.forEach(token => expect(has(token, 'value.color')).toEqual(false));
+    return;
+  });
+  it('Execute parser with filter', async () => {
+    const result = await omit(
+      seeds().tokens,
+      {
+        keys: ['value.color'],
+        filter: {
+          types: ['textStyle'],
+        },
+      },
+      libs,
+    );
+    if (result instanceof Error) return fail(result);
+    result.forEach(token => {
+      if (token.type === 'textStyle') expect(has(token, 'value.color')).toEqual(false);
+      if (token.type === 'border') expect(has(token, 'value.color')).toEqual(true);
+    });
+    return;
+  });
+  it('Execute parser with flatten option', async () => {
+    const tokens = seeds().tokens;
+    const result = await omit(
+      tokens,
+      {
+        keys: ['value'],
+        flatten: true,
+      },
+      libs,
+    );
+
+    if (result instanceof Error) return fail(result);
+    result.forEach(token => {
+      Object.keys(token).forEach(key => {
+        expect(token['value']).toBeFalsy();
+        expect(typeof token[key] !== 'object').toBeTruthy();
+      });
+    });
+    return;
+  });
+});
