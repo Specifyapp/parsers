6e3669631dce73fda9acf65f91459be2c20e41f2
Louis Chenais <lchenais@gmail.com>
2022-05-18 19:49:00 +0200

    feat(to-scss-variables): add to-scss-variables parser

--- parsers/to-css-custom-properties/to-css-custom-properties.spec.ts
+++ parsers/to-css-custom-properties/to-css-custom-properties.spec.ts
@@ -42,8 +42,8 @@ describe('To css', () => {
         }`,
       ),
     ).toBe(true);
-    return;
   });
+
   it('Get tokens - apply parsers - with options', async () => {
     const options: OptionsType = {
       formatName: 'snakeCase'!,
@@ -70,7 +70,6 @@ describe('To css', () => {
         `${fnFormatMeasurement}: ${measurement.value.measure}${measurement.value.unit}`,
       ),
     ).toBe(true);
-    return;
   });
 
   it('Get tokens - apply parsers - with custom selector', async () => {
@@ -103,7 +102,6 @@ describe('To css', () => {
       ),
     ).toBe(true);
     expect(result.includes(`${options.formatConfig!.selector}`)).toBe(true);
-    return;
   });
 
   it('Get tokens - apply parsers - without custom selector', async () => {
@@ -133,7 +131,6 @@ describe('To css', () => {
       ),
     ).toBe(true);
     expect(result.includes(`:root {`)).toBe(true);
-    return;
   });
 
   it('Get tokens - apply parsers - with camelCase', async () => {
@@ -166,50 +163,38 @@ describe('To css', () => {
       ),
     ).toBe(true);
     expect(result.includes(`:root {`)).toBe(true);
-    return;
   });
 
   it('Get tokens - apply parsers - all tokens', async () => {
-    const options: OptionsType = {};
+    const options: OptionsType = {
+      formatName: 'kebabCase'!,
+    };
     const result = await toCss(seeds().tokens, options, libs);
-    expect(result.includes('--heuristic-cross-platform-quantify: rgba(51, 15, 99, 0.6);')).toBe(
-      true,
-    );
+    expect(result.includes('--colors-accent: rgb(87, 124, 254);')).toBe(true);
     expect(
       result.includes(
-        '--open-system-markets-hard-drive: "https://specifyapp.com/_nuxt/img/881a6b6.webp";',
+        '--acme-logo: "https://s3-us-west-2.amazonaws.com/figma-alpha-api/img/13d6/2eb6/a2626d914998754ac0b704e2cdb7a813";',
       ),
     ).toBe(true);
-    expect(
-      result.includes('--invoice-system-worthy-payment: 7px solid rgba(199, 48, 37, 0.93);'),
-    ).toBe(true);
-    expect(result.includes('--digital-sas-avon: 805ms;')).toBe(true);
-    expect(result.includes('--frozen-withdrawal-gorgeous: Allan-Bold;')).toBe(true);
-    expect(result.includes('--paanga-calculate-plum: 73px;')).toBe(true);
-    expect(result.includes('--keyboard-fork-loaf: 0.13;')).toBe(true);
+    expect(result.includes('--border-accent: 2px solid rgba(102, 80, 239, 1);')).toBe(true);
+    expect(result.includes('--very-long: 3s;')).toBe(true);
+    expect(result.includes('--base-space-01: 4px;')).toBe(true);
+    expect(result.includes('--visible: 0.95;')).toBe(true);
+    expect(result.includes('--elevation-1: 0px 4px 8px rgba(0, 0, 0, 0.1);')).toBe(true);
     expect(
       result.includes(
-        '--car-ergonomic-licensed-cotton-pants: 37px 71px 2px rgba(144, 63, 6, 0.28);',
+        '--activity: "https://s3-us-west-2.amazonaws.com/figma-alpha-api/img/96dc/8825/c166b559140a0a64b28441924700a0b2";',
       ),
     ).toBe(true);
-    expect(result.includes('--kids-iowa-ergonomic: Allan-Bold 5pt rgba(86, 225, 86, 0.52);')).toBe(
-      true,
-    );
+    expect(result.includes('--background: 1;')).toBe(true);
     expect(
       result.includes(
-        '--handcrafted-rubber-computer-sky-blue-bandwidth: "https://raw.githubusercontent.com/feathericons/feather/master/icons/alert-circle.svg";',
+        `  --gradients-colored: linear-gradient(
+    90deg,
+    rgb(245, 72, 63) 0%,
+    rgb(255, 142, 5) 100%
+  );`,
       ),
     ).toBe(true);
-    expect(result.includes('--towels-uniform-tasty: 8;')).toBe(true);
-    expect(
-      result.includes(
-        '      --concrete-next-generation-palladium: linear-gradient(\n' +
-          '          212deg,\n' +
-          '          rgba(186, 149, 255, 0.34) 0%,\n' +
-          '          rgba(229, 120, 89, 0.79) 13%\n' +
-          '        ),\n',
-      ),
-    ).toBe(true);
-    return;
   });
 });
--- parsers/to-scss-variables/to-scss-variables.spec.ts
+++ parsers/to-scss-variables/to-scss-variables.spec.ts
@@ -103,40 +103,35 @@ describe('To scss', () => {
   });
 
   it.only('Get tokens - apply parsers - all tokens', async () => {
-    const options: OptionsType = {};
+    const options: OptionsType = {
+      formatName: 'kebabCase'!,
+    };
     const result = await toScss(seeds().tokens, options, libs);
-    console.log(result);
-    // expect(result.includes('$heuristic-cross-platform-quantify: rgba(51, 15, 99, 0.6);')).toBe(true);
-    // expect(
-    //   result.includes(
-    //     '$open-system-markets-hard-drive: "https://specifyapp.com/_nuxt/img/881a6b6.webp";',
-    //   ),
-    // ).toBe(true);
-    // expect(result.includes('$invoice-system-worthy-payment: 7px solid rgba(199, 48, 37, 0.93);')).toBe(true);
-    // expect(result.includes('$digital-sas-avon: 805ms;')).toBe(true);
-    // expect(result.includes('$frozen-withdrawal-gorgeous: Allan-Bold;')).toBe(true);
-    // expect(result.includes('$paanga-calculate-plum: 73px;')).toBe(true);
-    // expect(result.includes('$keyboard-fork-loaf: 0.13;')).toBe(true);
-    // expect(
-    //   result.includes(
-    //     '$car-ergonomic-licensed-cotton-pants: 37px 71px 2px rgba(144, 63, 6, 0.28);',
-    //   ),
-    // ).toBe(true);
-    // expect(result.includes('$kids-iowa-ergonomic: Allan-Bold 5pt rgba(86, 225, 86, 0.52);')).toBe(true);
-    // expect(
-    //   result.includes(
-    //     '$handcrafted-rubber-computer-sky-blue-bandwidth: "https://raw.githubusercontent.com/feathericons/feather/master/icons/alert-circle.svg";',
-    //   ),
-    // ).toBe(true);
-    // expect(result.includes('$towels-uniform-tasty: 8;')).toBe(true);
-    // expect(
-    //   result.includes(
-    //     '      $concrete-next-generation-palladium: linear-gradient(\n' +
-    //       '          212deg,\n' +
-    //       '          rgba(186, 149, 255, 0.34) 0%,\n' +
-    //       '          rgba(229, 120, 89, 0.79) 13%\n' +
-    //       '        ),\n',
-    //   ),
-    // ).toBe(true);
+    expect(result.includes('$colors-accent: rgb(87, 124, 254);')).toBe(true);
+    expect(
+      result.includes(
+        '$acme-logo: "https://s3-us-west-2.amazonaws.com/figma-alpha-api/img/13d6/2eb6/a2626d914998754ac0b704e2cdb7a813";',
+      ),
+    ).toBe(true);
+    expect(result.includes('$border-accent: 2px solid rgba(102, 80, 239, 1);')).toBe(true);
+    expect(result.includes('$very-long: 3s;')).toBe(true);
+    expect(result.includes('$base-space-01: 4px;')).toBe(true);
+    expect(result.includes('$visible: 0.95;')).toBe(true);
+    expect(result.includes('$elevation-1: 0px 4px 8px rgba(0, 0, 0, 0.1);')).toBe(true);
+    expect(
+      result.includes(
+        '$activity: "https://s3-us-west-2.amazonaws.com/figma-alpha-api/img/96dc/8825/c166b559140a0a64b28441924700a0b2";',
+      ),
+    ).toBe(true);
+    expect(result.includes('$background: 1;')).toBe(true);
+    expect(
+      result.includes(
+        `$gradients-colored: linear-gradient(
+  90deg,
+  rgb(245, 72, 63) 0%,
+  rgb(255, 142, 5) 100%
+);`,
+      ),
+    ).toBe(true);
   });
 });
