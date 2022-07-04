8f27689c69a179e68481e92eeef1a5055f8d0b1d
Antoine Moreaux <moreaux.antoine@gmail.com>
2022-07-04 13:42:13 +0200

    chore(tests): update global tests file (#157)

--- tests/global.ts
+++ tests/global.ts
@@ -4,49 +4,50 @@ import fs from 'fs';
 import crypto from 'crypto';
 import path from 'path';
 
+export const SpServices: Record<string, Record<string, Function>> = {
+  font: {
+    convert: async (payload: { postscriptName: string; formats: Array<AllowedFormat> }) => {
+      let result: PartialRecord<AllowedFormat, string> = {};
+      const inSeed = seeds().tokens.find(
+        token =>
+          token.type === 'font' &&
+          (token.value as FontToken['value']).fontPostScriptName === payload.postscriptName,
+      ) as FontToken;
+      if (inSeed && inSeed.value.fontFileMissing) {
+        throw new Error('Source file not found');
+      }
+      await Promise.all(
+        payload.formats.map(async format => {
+          result[format] = `${payload.postscriptName}.${format}`;
+        }),
+      );
+      return result;
+    },
+  },
+  assets: {
+    getSource<T>(url: string, responseType: 'text' | 'buffer' | 'json') {
+      try {
+        const content = fs.readFileSync(
+          path.join(
+            __dirname,
+            'fixtures',
+            'assets',
+            crypto.createHash('md5').update(url).digest('hex'),
+          ),
+        );
+        if (responseType === 'text') return content.toString('utf-8');
+        if (responseType === 'json') return JSON.parse(content.toString());
+        if (responseType === 'buffer') return content;
+      } catch (err) {
+        return new Error('Get source failed');
+      }
+    },
+  },
+};
 jest.mock('../parsers/global-libs', () => {
   const { default: globalLibs } = jest.requireActual('../parsers/global-libs');
   return {
     ...globalLibs,
-    SpServices: {
-      font: {
-        convert: async (payload: { postscriptName: string; formats: Array<AllowedFormat> }) => {
-          let result: PartialRecord<AllowedFormat, string> = {};
-          const inSeed = seeds().tokens.find(
-            token =>
-              token.type === 'font' &&
-              (token.value as FontToken['value']).fontPostScriptName === payload.postscriptName,
-          ) as FontToken;
-          if (inSeed && inSeed.value.fontFileMissing) {
-            throw new Error('Source file not found');
-          }
-          await Promise.all(
-            payload.formats.map(async format => {
-              result[format] = `${payload.postscriptName}.${format}`;
-            }),
-          );
-          return result;
-        },
-      },
-      assets: {
-        getSource<T>(url: string, responseType: 'text' | 'buffer' | 'json') {
-          try {
-            const content = fs.readFileSync(
-              path.join(
-                __dirname,
-                'fixtures',
-                'assets',
-                crypto.createHash('md5').update(url).digest('hex'),
-              ),
-            );
-            if (responseType === 'text') return content.toString('utf-8');
-            if (responseType === 'json') return JSON.parse(content.toString());
-            if (responseType === 'buffer') return content;
-          } catch (err) {
-            return new Error('Get source failed');
-          }
-        },
-      },
-    },
+    SpServices,
   };
 });
