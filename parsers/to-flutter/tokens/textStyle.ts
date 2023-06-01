a427af8630c62cb5e303658bdf9e4613a936531b
JulesGuesnon <jules.guesnon@hetic.net>
2023-06-01 23:14:55 +0900

    feat(to-flutter): Added text styles and variables types

---
+++ parsers/to-flutter/tokens/textStyle.ts
@@ -0,0 +1,70 @@
+import { OptionsType } from '../to-flutter.parser';
+import { O, pipe } from '@mobily/ts-belt';
+import Template from '../../../libs/template';
+import { DownloadableFile, TextDecorationValue, TextStyleToken } from '../../../types';
+import { colorToFlutter } from './color';
+import { camelCase } from 'lodash';
+
+const templateContent = new Template(`
+import 'dart:ui';
+
+class {{textStyleClass}} {
+    {{textStyleClass}}._();
+
+    {{#textStyles}}
+    static const {{#type}}{{type}} {{/type}}{{name}} = TextStyle(
+      fontFamily: '{{fontFamilly}}',
+      fontSize: {{fontSize}},
+      fontStyle: {{fontStyle}},
+      fontWeight: FontWeight.w{{fontWeight}},
+      {{#decoration}}decoration: {{decoration}},{{/decoration}}
+      {{#letterSpacing}}letterSpacing: {{letterSpacing}},{{/letterSpacing}}
+      {{#color}}color: Color({{color}}),{{/color}}
+    );
+    {{/textStyles}}
+}`);
+
+const textDecorationToFlutter = (textDecoration: TextDecorationValue): string => {
+  switch (textDecoration) {
+    case 'line-through':
+      return 'lineThrough';
+    case 'overline':
+    case 'underline':
+      return textDecoration;
+    default:
+      return 'none';
+  }
+};
+
+export function generateTextStyleFile(
+  textStyles: Array<TextStyleToken>,
+  options: OptionsType,
+): DownloadableFile {
+  return {
+    name: options?.formatByType?.color?.fileName ?? 'text-styles.dart',
+    value: {
+      content: templateContent.render({
+        textStyleClass: options?.formatByType?.color?.className ?? 'SpecifyTextStyle',
+        textStyles: textStyles.map(textStyle => ({
+          name: camelCase(textStyle.name),
+          type: options?.formatByType?.textStyles?.classType ?? 'TextStyle',
+          fontFamilly: textStyle.value.font.value.fontFamily,
+          fontSize: textStyle.value.fontSize.value.measure.toFixed(2),
+          fontStyle: `FontStyle.${!!textStyle.value.font.value.isItalic ? 'italic' : 'normal'}`,
+          fontWeight: textStyle.value.font.value.fontWeight,
+          decoration: pipe(
+            O.fromNullable(textStyle.value.textDecoration?.[0]),
+            O.map(v => `TextDecoration.${textDecorationToFlutter(v)}`),
+            O.toUndefined,
+          ),
+          letterSpacing: textStyle.value.letterSpacing?.value.measure.toFixed(2),
+          color: pipe(
+            O.fromNullable(textStyle.value.color?.value),
+            O.map(colorToFlutter),
+            O.toUndefined,
+          ),
+        })),
+      }),
+    },
+  };
+}
