07c50e66e177873265ebfbdd0ef231a1af75f3cd
Antoine <antoine@specifyapp.com>
2023-06-17 18:00:45 +0200

    fix(to-flutter): fix filename option for textStyle

--- parsers/to-flutter/tokens/textStyle.ts
+++ parsers/to-flutter/tokens/textStyle.ts
@@ -11,13 +11,13 @@ import 'dart:ui';
 class {{textStyleClass}} {
     {{textStyleClass}}._();
 
-    {{#fontFamillies}}
+    {{#fontFamilies}}
     static const String {{varName}} = '{{fontName}}';
-    {{/fontFamillies}}
+    {{/fontFamilies}}
 
     {{#textStyles}}
     static const {{#type}}{{type}} {{/type}}{{name}} = TextStyle(
-      fontFamily: {{fontFamilly}},
+      fontFamily: {{fontFamily}},
       fontSize: {{fontSize}},
       fontStyle: {{fontStyle}},
       fontWeight: FontWeight.w{{fontWeight}},{{#decoration}}\n      decoration: {{decoration}},{{/decoration}}{{#letterSpacing}}\n      letterSpacing: {{letterSpacing}},{{/letterSpacing}}{{#color}}\n      color: Color({{color}}),{{/color}}
@@ -42,11 +42,11 @@ export function generateTextStyleFile(
   options: OptionsType,
 ): DownloadableFile {
   return {
-    name: options?.formatByType?.color?.fileName ?? 'text-styles.dart',
+    name: options?.formatByType?.textStyle?.fileName ?? 'text-styles.dart',
     value: {
       content: templateContent.render({
-        textStyleClass: options?.formatByType?.color?.className ?? 'SpecifyTextStyle',
-        fontFamillies: [...new Set(textStyles.map(v => v.value.font.value.fontFamily))].map(
+        textStyleClass: options?.formatByType?.textStyle?.className ?? 'SpecifyTextStyle',
+        fontFamilies: [...new Set(textStyles.map(v => v.value.font.value.fontFamily))].map(
           fontName => ({
             fontName,
             varName: `_${camelCase(fontName)}`,
@@ -55,7 +55,7 @@ export function generateTextStyleFile(
         textStyles: textStyles.map(textStyle => ({
           name: camelCase(textStyle.name),
           type: options?.formatByType?.textStyle?.classType ?? 'TextStyle',
-          fontFamilly: `_${camelCase(textStyle.value.font.value.fontFamily)}`,
+          fontFamily: `_${camelCase(textStyle.value.font.value.fontFamily)}`,
           fontSize: textStyle.value.fontSize.value.measure.toFixed(2),
           fontStyle: `FontStyle.${!!textStyle.value.font.value.isItalic ? 'italic' : 'normal'}`,
           fontWeight: textStyle.value.font.value.fontWeight,
